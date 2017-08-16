f3b1e2969c8419aa4efeb2e838da1dd62fc593c5
pongsatorn manusopit <ton11797@hotmail.com>
2017-08-16 14:09:51 +0700

    form

--- src/components/AddGenericMedicineAndSupplies.js
+++ src/components/AddGenericMedicineAndSupplies.js
@@ -1,34 +1,14 @@
 import React , { Component } from 'react';
 import {
-  Row,
-  Col,
-  PanelContainer,
-  Panel,
-  PanelBody,
-  Grid,
   Button,
 } from '@sketchpixy/rubix';
 class AddGenericMedicineAndSupplies extends Component {
   render(){
     return(
       <div>
-        <Row>
-          <Col xs={12}>
-            <PanelContainer>
-              <Panel>
-                <PanelBody>
-                  <Grid>
-                    <Row>
-                      <Col xs={12}>
-                        <Button bsStyle='primary' onClick={this.props.submit}>submit</Button>
-                      </Col>
-                    </Row>
-                  </Grid>
-                </PanelBody>
-              </Panel>
-            </PanelContainer>
-          </Col>
-        </Row>
+        <Button bsStyle='primary' onClick={this.props.submit}>submit</Button>
+        <br/>
+        test
       </div>
     );
   }
