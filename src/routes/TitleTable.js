5f9111650c66d8a1cd18f61e836736cc8c96790b
kik <30797290+tunchanoklur@users.noreply.github.com>
2017-08-18 15:34:51 +0700

    Title Table added

---
+++ src/routes/TitleTable.js
@@ -0,0 +1,153 @@
+import React from "react";
+import { render } from "react-dom";
+// Import React Table
+import ReactTable from "react-table";
+import {
+    Row,
+    Col,
+    Grid,
+    Panel,
+    Table,
+    PanelBody,
+    PanelHeader,
+    FormControl,
+    PanelContainer,
+    DropdownButton,
+    DropdownHoverButton,
+    MenuItem,
+    ButtonToolbar,
+    ButtonGroup,
+    Button,
+    Modal,
+    Form,
+    FormGroup,
+    InputGroup,
+    ControlLabel,
+    Icon,
+} from '@sketchpixy/rubix';
+
+class Titleform extends React.Component {
+    render() {
+        return (
+            <Form>
+                <Row>
+                <Col xs={6} md={6}>
+                    <FormGroup controlId="Title">
+                    <ControlLabel>คำนำหน้านาม</ControlLabel>
+                    <FormControl type="text" placeholder="Title"/>
+                    </FormGroup>
+                </Col>
+            </Row>
+        </Form>
+        );
+    }
+}
+
+class Buttonbar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { showModal: false };
+    }
+
+    close() {
+        this.setState({ showModal: false });
+    }
+
+    open() {
+        this.setState({ showModal: true });
+    }
+    render() {
+        return (
+    <div>
+    <Button bsStyle='success' onClick={::this.open}><Icon glyph='icon-fontello-plus'/>  เพิ่มคำนำหน้านาม</Button>
+    <Modal show={this.state.showModal} onHide={::this.close}>
+      <Modal.Header closeButton>
+      <Modal.Title>เพิ่ม/แก้ไข คำนำหน้านาม</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Titleform/>
+      </Modal.Body>
+      <Modal.Footer>
+      <Button bsStyle='success' onClick={::this.close}>บันทึก</Button>
+      <Button onClick={::this.close}>ยกเลิก</Button>
+      </Modal.Footer>
+    </Modal>
+
+    <ButtonGroup className='pull-right'>
+          <Button bsStyle='primary' >
+          <Icon glyph='icon-fontello-download' /> นำออกไฟล์
+          </Button>
+          <Button bsStyle='defalt'>
+          <Icon glyph='icon-fontello-print' /> พิมพ์
+          </Button>
+    </ButtonGroup>
+    </div>
+        );
+    }
+}
+
+class Titletable extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          data: [
+              {
+                title:"ศาสตราจารย์",
+                delete:<Button bsStyle='danger'><Icon glyph='icon-fontello-trash'/>  ลบ</Button>
+              }
+          ]
+        };
+      }
+
+    render() {
+        return (
+            <ReactTable
+            data={this.state.data}
+            noDataText="ไม่พบข้อมูล"
+            filterable
+            defualtFilterMethod={(filter, row)=>
+                String(row[filter.id])===filter.value ||
+                row[filter.id].includes(filter.value)}
+            columns={[
+              {
+                Header: "คำนำหน้านาม",
+                accessor: "title"
+              },
+              {
+                Header: "การแก้ไข",
+                accessor:"delete"
+              }
+            ]}
+            defaultPageSize={10}
+            className="-striped -highlight"
+          />
+        );
+    }
+}
+export default class TitleTable extends React.Component {
+    render() {
+        return (
+            <Row>
+        <Col xs={12}>
+          <PanelContainer>
+            <Panel>
+              <PanelBody>
+                <Grid>
+                  <Row>
+                    <Col xs={12}>
+                      <Buttonbar/>
+                      <hr/>
+                      <Titletable />
+                      <br/>
+                    </Col>
+                  </Row>
+                </Grid>
+              </PanelBody>
+            </Panel>
+          </PanelContainer>
+        </Col>
+      </Row>
+        );
+    }
+}
